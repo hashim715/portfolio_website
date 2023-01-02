0484f8482d7b7fa817634a1988a335da8e84a4c9
hashim715 <mharus575@gmail.com>
2023-01-02 22:49:15 +0500

    added changes

--- src/data.js
+++ src/data.js
@@ -5,19 +5,19 @@ import IMG3 from "./assets/hashim-portfolio-3.png";
 const data = [
   {
     id: 1,
-    image: IMG1,
+    image: IMG2,
     title: "DesiFirms",
     demo: "https://djangodesifirms.herokuapp.com/",
   },
   {
     id: 2,
-    image: IMG2,
+    image: IMG3,
     title: "Ecommerce App",
     demo: "https://shoppingapp.herokuapp.com/",
   },
   {
     id: 3,
-    image: IMG3,
+    image: IMG1,
     title: "Waseelaeawal Website",
     demo: "https://waseelaeawal.herokuapp.com/",
   },
