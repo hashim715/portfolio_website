0205344e45d2051f46a353bb985e5c9fb509d78e
hashim715 <mharus575@gmail.com>
2023-01-02 22:39:10 +0500

    added changes

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I am</h5>
         <h1>Muhammad Hashim</h1>
-        <h5 className="text-light">Front-End Developer & Content Designer</h5>
+        <h5 className="text-light">Full Stack Developer</h5>
 
         <Buttons />
         <HeaderSocials />
