50b2e6ea976ed22f1969d6e36c9d9ff4a77ac088
hashim715 <60181698+hashim715@users.noreply.github.com>
2023-09-08 06:34:10 -0700

    Update data.js
    updated changes

--- src/data.js
+++ src/data.js
@@ -3,24 +3,6 @@ import IMG2 from "./assets/hashim-portfolio-2.png";
 import IMG3 from "./assets/hashim-portfolio-3.png";
 
 const data = [
-  {
-    id: 1,
-    image: IMG2,
-    title: "DesiFirms",
-    demo: "https://djangodesifirms.herokuapp.com/",
-  },
-  {
-    id: 2,
-    image: IMG3,
-    title: "Ecommerce App",
-    demo: "https://shoppingapp.herokuapp.com/",
-  },
-  {
-    id: 3,
-    image: IMG1,
-    title: "Waseelaeawal Website",
-    demo: "https://waseelaeawal.herokuapp.com/",
-  },
 ];
 
 export default data;
