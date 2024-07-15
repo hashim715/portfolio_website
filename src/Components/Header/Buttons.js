2500423c559ff66aa8662eb386bac601e5cb7aba
Muhammad Hashim <60181698+hashim715@users.noreply.github.com>
2024-07-15 13:11:09 +0530

    Update Buttons.js

--- src/Components/Header/Buttons.js
+++ src/Components/Header/Buttons.js
@@ -4,7 +4,7 @@ import CV from "../../assets/hashim_cv.pdf";
 const Buttons = () => {
   return (
     <div className="cta">
-      <a href={CV} download className="btn">
+      <a href="#" download className="btn">
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
