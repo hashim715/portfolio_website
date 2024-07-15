386195328f94d3899befdefa7c2627ddbe3a1058
Muhammad Hashim <60181698+hashim715@users.noreply.github.com>
2024-07-15 13:17:08 +0530

    Update Buttons.js

--- src/Components/Header/Buttons.js
+++ src/Components/Header/Buttons.js
@@ -4,7 +4,7 @@ import CV from "../../assets/hashim_cv.pdf";
 const Buttons = () => {
   return (
     <div className="cta">
-      <a href="#" download className="btn">
+      <a href="https://collection.cloudinary.com/dicdsctqj/668c03ea23347b906368b0d438bfe4aa" download className="btn">
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
