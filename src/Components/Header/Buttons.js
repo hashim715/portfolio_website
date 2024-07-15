d947c5ed3f3b34487ca738ee0ec979bf2b1b8791
Muhammad Hashim <60181698+hashim715@users.noreply.github.com>
2024-07-15 13:35:25 +0530

    Update Buttons.js

--- src/Components/Header/Buttons.js
+++ src/Components/Header/Buttons.js
@@ -3,7 +3,7 @@ import React from "react";
 const Buttons = () => {
   return (
     <div className="cta">
-      <a href="https://res.cloudinary.com/dicdsctqj/image/upload/v1721030095/my_assets/lg6gbiiz0trdxue5vllu.pdf" download className="btn">
+      <a href="https://res.cloudinary.com/dicdsctqj/image/upload/v1721030697/my_assets/vnwjumgeb2nqkf05vbv3.jpg" download className="btn">
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
